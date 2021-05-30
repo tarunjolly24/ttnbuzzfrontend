960905f27fcce2fb8d6313bf28ba883e030b113e
Tarun Jolly <tarun.jolly@tothenew.com>
2021-05-30 07:52:51 +0530

    href set
    Signed-off-by: Tarun Jolly <tarun.jolly@tothenew.com>

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance= axios.create({
-    baseURL:'http://localhost:5000/'
-    // baseURL:'https://ttnbuzzbackend.el.r.appspot.com/'
+    // baseURL:'http://localhost:5000/'
+    baseURL:'https://ttnbuzzbackend.el.r.appspot.com/'
 })
 
 instance.interceptors.request.use(config => {
